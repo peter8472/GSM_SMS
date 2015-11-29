00e987dc41dd2fb72c462d5bd215812585f40cd1
peter8472 <pmgcal@outlook.com>
2015-11-28 21:24:00 -0800

    Update gsm7.py

--- gsm7.py
+++ gsm7.py
@@ -6,8 +6,10 @@ https://en.wikipedia.org/wiki/GSM_03.38
 https://en.wikipedia.org/wiki/GSM_03.40#Message_Content
 http://www.smartposition.nl/resources/sms_pdu.html
 http://www.canarysystems.com/nsupport/CDMA_AT_Commands.pdf
-bit format is here:
+7bit format is here:
 http://www.etsi.org/deliver/etsi_ts/100900_100999/100900/07.02.00_60/ts_100900v070200p.pdf
+The offical AT commands for SMS are here:
+http://www.etsi.org/deliver/etsi_gts/07/0705/05.00.00_60/gsmts_0705v050000p.pdf
 
 
 """
