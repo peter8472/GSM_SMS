8c0956d357c0dfaa25b425f14e9207b6b63d5fa9
peter8472 <pmgcal@outlook.com>
2015-12-01 19:03:27 -0800

    Update gsm7.py

--- gsm7.py
+++ gsm7.py
@@ -1,68 +1,44 @@
 #! /usr/bin/python
-import csv
-import pdb
-"""This package looks up gsm7 special alphabet
-https://en.wikipedia.org/wiki/GSM_03.38
-https://en.wikipedia.org/wiki/GSM_03.40#Message_Content
-http://www.smartposition.nl/resources/sms_pdu.html
-http://www.canarysystems.com/nsupport/CDMA_AT_Commands.pdf
-7bit format is here:
-http://www.etsi.org/deliver/etsi_ts/100900_100999/100900/07.02.00_60/ts_100900v070200p.pdf
-The offical AT commands for SMS are here:
-http://www.etsi.org/deliver/etsi_gts/07/0705/05.00.00_60/gsmts_0705v050000p.pdf
+
+"""Another gsm lookup class.  This one should actually work"""
+b=      [u"@∆ 0¡P¿p"]
+b.append(u"£_!1AQaq")
+b.append(u'$Φ"2BRbr')
+b.append(u"¥Γ#3CScs")
+b.append(u"èΛ¤4DTdt")
+b.append(u"éΩ%5EUeu")
+b.append(u"ùΠ&6FVfv")
+b.append(u"ìΨ'7GWgw")
+b.append(u"òΣ(8HXhx")
+b.append(u"ÇΘ)9IYiy")
+b.append(u"\nΞ*:JZjz")
+b.append(u"Ø +;KÄkä") #no character here, it's an escape
+b.append(u"øÆ,<LÖlö")
+b.append(u"\ræ-=MÑmñ")
+b.append(u"Åß.>NÜnü")
+b.append(u"åÉ/?O§oà")
 
 
-"""
-alpha = {}    
 class Gsm7(object):
     def __init__(self):
-        self.alpha = {}
-        with open("gsm.csv") as f:
-            j = csv.reader(f)
-            for i in j:
-                self.alpha[int(i[0], 16)] = i[1]
-
-    def write_all(self):
-        f = open("gsm.csv", 'a')
-        j = csv.writer(f)
-        sorted = self.alpha.keys()
-        sorted.sort()
-        for a in sorted:
-            
-            j.writerow(["{0:#x}".format(a), self.alpha[a]])
-            print ["{0:#x}".format(a), self.alpha[a]]
-        f.close()
-
-    def look(self,codepoint):
-        if self.alpha.has_key(codepoint):
-            return self.alpha[codepoint]
-        else:
-            self.alpha[codepoint] = ""
-            print "in OOKUP"
-            f = open("gsm.csv", 'a')
-            f.write("{0:#x},\n".format(codepoint))
-            f.close()
-            return "?"
+        pass
 
 
-def blah():
-    f = open("gsm.csv", 'w')
-    j = csv.writer(f)
-    for a in alpha:
-        j.writerow(["{0:#x}".format(a), alpha[a]])
-    f.close()
+    def look(self, codepoint):
+        row = codepoint & 0b0001111
+        col = codepoint >> 4
+        #print "%d:%d"% (row, col)
+        if row == 11 and col == 1:
+            raise Exception("gsm escape char found")
+        try:
+            return b[row][col]
+        except IndexError(e):
+            print e
+            exit(1)
 
-def sp(number):
-    print "{0:0x} {0:08b}".format(number)
 
 if __name__ == "__main__":
-    tstr = [0x44,0x65,0x73,0x69, 0x67, 0x6e,0x00,0x48,0x6f,0x6d,0x65]
-    #let's pack that... okay...
-    
-    tmp = tstr[0]
-    sp(tmp)
-    tmp = tmp << 1
-    sp(tmp)
-    tmp = tmp | tstr[1] >> 7
-    sp(tmp)
-    
+    mygsm = Gsm7()
+    for x in range(0,3):
+      print mygsm.look(x)
+      print b[7][6]
